e3895c8fc4d47593fed64ff3c76d0149b439254c
mariatt25pmc137-dot <mariatt.25pmc137@mariancollege.org>
2026-07-22 13:03:26 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,13 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 7;
+        
+        System.out.println("Multiplication Table for " + number + ":");
+        System.out.println("---------------------------------");
+        
+        // Loop from 1 to 10 to display the table
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
